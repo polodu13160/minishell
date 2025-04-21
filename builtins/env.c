c1222d1e0acf04c5179c4bf59e22cc8f87e6d371
Polo13160 <paul.depetrini@gmail.com>
2025-04-21 17:37:58 +0200

    mis de cote

--- builtins/env.c
+++ builtins/env.c
@@ -6,14 +6,18 @@
 /*   By: pde-petr <pde-petr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 12:28:18 by pde-petr          #+#    #+#             */
-/*   Updated: 2025/04/16 14:01:21 by pde-petr         ###   ########.fr       */
+/*   Updated: 2025/04/16 14:14:19 by pde-petr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/includes/libft.h"
+#include "libft.h"
 #include "stdlib.h"
 
-int	ft_env(char **env)
+/// @brief print env and export table
+/// @param env is the env
+/// @param set is all variables export
+/// @return
+int	ft_env(char **env, char **set)
 {
 	int	i;
 
@@ -22,6 +26,14 @@ int	ft_env(char **env)
 		while (env[i])
 		{
 			ft_putstr_fd(env[i], 1);
+			ft_putchar_fd('\n', 1);
+			i++;
+		}
+	i = 0;
+	if (set != NULL)
+		while (set[i])
+		{
+			ft_putstr_fd(set[i], 1);
 			ft_putchar_fd('\n', 1);
 			i++;
 		}
