191406444b8df2dab3a052ad2a94af1782ae5763
Polo13160 <paul.depetrini@gmail.com>
2025-06-29 02:50:19 +0200

    finish correction for $variable:$variable or other things in this problem

--- sources/parsing_get_env.c
+++ sources/parsing_get_env.c
@@ -12,6 +12,40 @@
 
 #include "token.h"
 
+char *ft_join_malloc(char *malloc_str1, char *malloc_str2)
+{
+	char *join;
+	if (!malloc_str1)
+	{
+
+		if (malloc_str2)
+			free(malloc_str2);
+		return NULL;
+	}
+	if (!malloc_str2)
+	{
+
+		if (malloc_str1)
+			free(malloc_str1);
+		return NULL;
+	}
+	join = ft_strjoin(malloc_str1,malloc_str2);
+	free(malloc_str1);
+	free(malloc_str2);
+	if (join == NULL)
+		return NULL;
+	return join;
+}
+char *ft_chardup(char c)
+{
+	char *malloc_char;
+	malloc_char = ft_calloc(2,sizeof(char));
+	if (malloc_char == NULL)
+		return NULL;
+	*malloc_char = c;
+	return malloc_char;
+}
+
 char	*get_env_value(char *var_name, char **env)
 {
 	int		i;
@@ -23,6 +57,7 @@ char	*get_env_value(char *var_name, char **env)
 	if (!env)
 		return (ft_strdup("\0"));
 	name_len = ft_strlen(var_name);
+
 	if (name_len == 0)
 		return (ft_strdup("$"));
 	i = 0;
@@ -40,17 +75,89 @@ char	*get_env_value(char *var_name, char **env)
 	}
 	return (ft_strdup("\0"));
 }
+//antoine ton extract var n peut pas etre adapter ailleur il faudrait..
+char *extract_var_env(char *str)
+{
+	char *str_save;
+	char *null_str_save;
 
+
+	while (*str != '$' && *str != '\0')
+		str++;
+	if (*str == '\0')
+		return NULL;
+	str_save = ft_strdup(str);
+	if (str_save == NULL)
+		return NULL;
+	null_str_save = str_save + 1;
+	while (*null_str_save != '"' && *null_str_save != '\'' && *null_str_save != '$' && *null_str_save != '/' && *null_str_save != ':' && *null_str_save != '\0' && *null_str_save != '?')
+		null_str_save++;
+	while (*null_str_save)
+		*(null_str_save++) = '\0';
+	return str_save;
+}
+//est ce que cela protege au dessus ?si retourne null
 char	*return_env(char *str, t_minishell *minishell)
 {
-	char	*var_name;
+	static char	*var_name = NULL;
+	char *var_env;
 
-	if (!str)
-		return (NULL);
-	var_name = str + 1;
-	if (ft_strncmp(var_name, "?", 2) == 0)
-		return (ft_itoa(minishell->return_command));
-	return (get_env_value(var_name, minishell->env));
+	char *return_value;
+	var_env = NULL;
+
+	if (*str == '\0')
+	{
+		return_value = var_name;
+		var_name = NULL;
+		return return_value;
+	}
+
+	if (ft_strncmp(str, "$?", 2) == 0)
+	{
+
+		str++;
+		str++;
+		if (*(str+2) != '\0' && var_name == NULL)
+			var_name = ft_itoa(minishell->return_command);
+		else
+			var_name = ft_join_malloc(var_name,ft_itoa(minishell->return_command));
+		if (var_name == NULL)
+			return NULL;
+	}
+	if (var_name == NULL)
+          var_name = ft_strdup("");
+	while (*str != '\0' && *str != '$')
+	{
+		var_name = ft_join_malloc(var_name, ft_chardup(*str));
+		str++;
+	}
+	if (*str == '$')
+	{
+		if (*(str+1) == '$')
+		{
+			str++;
+			str++;
+			var_name = ft_join_malloc(var_name, ft_strdup("$$"));
+		}
+		else
+		{
+			var_env = extract_var_env(str);
+			if (var_env == NULL)
+			{
+				if (var_name)
+					free(var_name);
+				return NULL;
+			}
+			var_name = ft_join_malloc(var_name, get_env_value(var_env+1, minishell->env));
+			str++;
+			while (*str != '"' && *str != '\'' && *str != '$' && *str != '/' && *str != ':' && *str != '\0' && *str != '?')
+					str++;
+		}
+	}
+	free(var_env);
+	if (var_name == NULL)
+		return NULL;
+	return return_env(str,minishell);
 }
 
 void	copy_single(char *str, char *result, int *i, int *j)
